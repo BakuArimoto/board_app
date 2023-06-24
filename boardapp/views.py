51d4047d8b4366b142e0114a6b740b3b0dafd44c
Baku Arimoto <Baku.Arimoto@gmail.com>
2023-06-24 11:18:16 +0900

    20230623_final

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signupfunc(request):
+    if request.method == "POST":
+        print(request.POST)
+        print('helo')
+        # username = request.POST["username"]
+        # password = request.POST["password"]
+
+        # username = request.POST.get('username')
+        # password = request.POST.get('password') 
+        # print(request.POST)
     return render(request, 'signup.html', {'some':100})
